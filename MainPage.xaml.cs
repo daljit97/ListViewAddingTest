8552fdbd7a8acd32aee71bd689425410e5886013
Daljit <daljit97@live.com>
2020-01-30 13:35:32 +0000

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Input.Inking;
+
+namespace ListViewAddingTest
+{
+    public sealed partial class MainPage : Page
+    {
+        ObservableCollection<ViewModel> pageCollection;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            pageCollection = new ObservableCollection<ViewModel>();
+            CanvasListView.ItemsSource = pageCollection;
+            CanvasListView.IsHitTestVisible = true;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Adding new page...");
+            pageCollection.Add(new ViewModel());
+            PageCountText.Text = pageCollection.Count + " Pages";
+            Debug.WriteLine("CanvasListView count is " + CanvasListView.Items.Count);
+        }
+    }
+}
---
+++ ViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ListViewAddingTest
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
